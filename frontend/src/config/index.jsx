c38659e20ef74a962cd990882a3e33cca46c69b7
Mohd Adil <169131020+mohd-adil-2005@users.noreply.github.com>
2026-02-11 03:19:27 +0530

    Fix BASE_URL definition in config file

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 
   export const BASE_URL= "https://proconnectp.onrender.com/";
+https://proconnectp.onrender.com
   
 // export const BASE_URL= "http://localhost:8080/";
   export const clientServer= axios.create({
